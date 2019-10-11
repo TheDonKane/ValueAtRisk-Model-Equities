995e9fa0b3d6bd348a96c2c7acaf50bc15468086
Luke Shirley <Lukeshirley3@gmail.com>
2019-10-11 16:38:06 -0500

    Update Model.py
    Completed fully functioning VaR model

--- Model.py
+++ Model.py
@@ -9,6 +9,9 @@ import seaborn
 #Data fetching
 import yfinance as yf
 
+#Print tabular data
+from tabulate import tabulate
+
 #Calculate daily returns
 df = yf.download('GOOG')
 df = yf.download('MSFT')
@@ -20,3 +23,13 @@ plt.xlabel('Returns')
 plt.ylabel('Frequency')
 plt.grid(True)
 plt.show()
+
+#Sort the returns
+df.sort_values('returns', inplace = True, ascending = True)
+
+#Calculate Value at Risk
+VaR_90 = df['returns'].quantile(0.1)
+VaR_95 = df['returns'].quantile(0.05)
+VaR_99 = df['returns'].quantile(0.01)
+
+print (tabulate([['Confidence Level', 'Value at Risk'], ['90%', VaR_90], ['95%', VaR_95], ['99%', VaR_99]], headers ="firstrow"))
