632b59d16ebf98491b8b5a4b8f1e71b5fc32a92b
Luke Shirley <Lukeshirley3@gmail.com>
2019-10-11 12:59:14 -0500

    Create Model.py
    For now I will use yahoo finance but I plan to add either Quandl or AlphaVantage as the historical data API

---
+++ Model.py
@@ -0,0 +1,13 @@
+#Data manipulation
+import numpy as np
+import pandas as pd
+
+#Plotting
+import matplotlib.pyplot as plt
+import seaborn
+
+#Data fetching
+import fix_yahoo_finance as yf
+
+#Print tabular data
+from tabulate import tabulate
